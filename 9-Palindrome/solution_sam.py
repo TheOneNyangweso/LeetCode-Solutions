a5a554c6f5fdd828a0ef1b9688547f206632a2ec
TheOneNyangweso <morurisammy5@gmail.com>
2023-11-08 21:50:47 +0300

    "Follow up solution added as comment"

--- 9-Palindrome/solution_sam.py
+++ 9-Palindrome/solution_sam.py
@@ -1,8 +1,15 @@
 class Solution:
-    def rev_int(x : int)
     def isPalindrome(self, x: int) -> bool:
         x = str(x)
         y = x[::-1]
+        # Follow up: Could you solve it without converting the integer to a string? 
+        # Solution...
+        # y = 0
+        # n = x / 1
+        # while(n > 0):
+        #     a = n % 10
+        #     y = y * 10 + a
+        #     n = n // 10
         if x == y:
             return True
         else:
