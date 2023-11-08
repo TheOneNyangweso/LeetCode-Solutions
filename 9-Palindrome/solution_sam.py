40080b06c9d3b9dcff4b825deb541e5a78bb4b52
TheOneNyangweso <morurisammy5@gmail.com>
2023-11-08 21:31:24 +0300

    "added solution"

---
+++ 9-Palindrome/solution_sam.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rev_int(x : int)
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        y = x[::-1]
+        if x == y:
+            return True
+        else:
+            return False
