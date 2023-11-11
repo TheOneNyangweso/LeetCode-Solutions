2ed43cf0c3ec6833a8d322b58ab29bd57e13d25f
TheOneNyangweso <morurisammy5@gmail.com>
2023-11-11 23:03:10 +0300

    added solution to problem

---
+++ 14-Longest-Common-Prefix/solution_sam.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        first_val = strs[0]
+        
+        for letter in first_val:
+            for s in strs:
+                if s.startswith(res + letter):
+                    continue
+                else:
+                    return res
+            res += letter
+
+        return res
+                
