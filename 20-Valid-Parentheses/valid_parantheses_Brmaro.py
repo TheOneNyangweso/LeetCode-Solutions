a6a80b6b7b3d1803668f8daa06ddf82526f57065
BrMaro <70138725+BrMaro@users.noreply.github.com>
2023-11-13 23:56:24 +0300

    Update valid_parantheses_Brmaro.py

--- 20-Valid-Parentheses/valid_parantheses_Brmaro.py
+++ 20-Valid-Parentheses/valid_parantheses_Brmaro.py
@@ -1,23 +1,21 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
+    def isValid(self,s):
         stack = []
+        if sum([1 for b in s if b in "{[("]) != sum([1 for b in s if b in "}])"]):
+            return False
         if s[0] == "}" or s[0] == "]" or s[0] == ")":
             return False
-        if len(s) %2==0:
+        if len(s) % 2 == 0:
             for bracket in s:
                 if bracket == "{" or bracket == "(" or bracket == "[":
                     stack.append(bracket)
-                    print(1)
                 if not stack and (bracket == "}" or bracket == ")" or bracket == "]"):  # once we encounter a closing it is automatically a False
-                    print(2)
                     return False
-                elif bracket == "}" and stack[-1] == "{" and len(stack) > 0:
-                    print(3)
+                elif bracket == "}" and stack[-1] == "{" and stack:
                     stack.pop()
-                elif bracket == ")" and stack[-1] == "(" and len(stack) > 0:
-                    print(4)
+                elif bracket == ")" and stack[-1] == "(" and stack:
                     stack.pop()
-                elif bracket == "]" and stack[-1] == "[" and len(stack) > 0:
+                elif bracket == "]" and stack[-1] == "[" and stack:
                     print(5)
                     stack.pop()
             return True if len(stack) == 0 else False
